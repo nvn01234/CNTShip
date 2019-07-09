c2de90bf36d0083634acd0cb64482d5d0e46269d
Nguyen Van Nhat <nguyenvannhat152@gmail.com>
2019-07-10 01:21:55 +0700

    add radio button

--- components/CreateOrderScreen/CreateOrderForm.js
+++ components/CreateOrderScreen/CreateOrderForm.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {TextInput, RadioButton } from 'react-native-paper'
-import { StyleSheet, View} from "react-native";
+import { StyleSheet, View, Text} from "react-native";
 import Colors from '@constants/Colors'
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
 import {formatNumber} from "@utils";
+import PaymentTypes from '@constants/PaymentTypes'
+import ShipFeeTypes from "@constants/ShipFeeTypes";
 
 export default class CreateOrderForm extends React.Component {
     constructor(props) {
@@ -19,14 +21,14 @@ export default class CreateOrderForm extends React.Component {
             <KeyboardAwareScrollView style={styles.container}
                                      enableOnAndroid={true}
                                      enableAutoAutomaticScroll={true}
-                                     extraScrollHeight={100}
+                                     extraScrollHeight={120}
                                      viewIsInsideTabBar={true}>
                 <View>
                     <TextInput
                         style={styles.input}
                         label='Địa chỉ lấy hàng'
                         value={this.state.formData.shop_address}
-                        onChangeText={this._onChangeText('shop_address')}
+                        onChangeText={this._onChangeField('shop_address')}
                         underlineColor={Colors.inputUnderline}
                         selectionColor={Colors.inputSelection}
                     />
@@ -34,7 +36,7 @@ export default class CreateOrderForm extends React.Component {
                         style={styles.input}
                         label='Mô tả đơn hàng'
                         value={this.state.formData.description}
-                        onChangeText={this._onChangeText('description')}
+                        onChangeText={this._onChangeField('description')}
                         underlineColor={Colors.inputUnderline}
                         selectionColor={Colors.inputSelection}
                     />
@@ -42,7 +44,7 @@ export default class CreateOrderForm extends React.Component {
                         style={styles.input}
                         label='Họ tên người nhận'
                         value={this.state.formData.customer_name}
-                        onChangeText={this._onChangeText('customer_name')}
+                        onChangeText={this._onChangeField('customer_name')}
                         underlineColor={Colors.inputUnderline}
                         selectionColor={Colors.inputSelection}
                     />
@@ -59,34 +61,61 @@ export default class CreateOrderForm extends React.Component {
                         style={styles.input}
                         label='Địa chỉ người nhận'
                         value={this.state.formData.customer_address}
-                        onChangeText={this._onChangeText('customer_address')}
+                        onChangeText={this._onChangeField('customer_address')}
                         underlineColor={Colors.inputUnderline}
                         selectionColor={Colors.inputSelection}
                     />
-                    <TextInput
-                        style={styles.input}
-                        label='Giá trị đơn hàng'
-                        value={this.state.formData.total ? formatNumber(this.state.formData.total) : ''}
-                        onChangeText={this._onAmountChanged('total')}
-                        underlineColor={Colors.inputUnderline}
-                        selectionColor={Colors.inputSelection}
-                        keyboardType={'numeric'}
-                    />
-                    <TextInput
-                        style={styles.input}
-                        label='Phí ship'
-                        value={this.state.formData.ship_fee ? formatNumber(this.state.formData.ship_fee) : ''}
-                        onChangeText={this._onAmountChanged('ship_fee')}
-                        underlineColor={Colors.inputUnderline}
-                        selectionColor={Colors.inputSelection}
-                        keyboardType={'numeric'}
-                    />
+                    <View style={styles.inline}>
+                        <TextInput
+                            style={[styles.input, styles.flex2]}
+                            label='Giá trị đơn hàng'
+                            value={this.state.formData.total ? formatNumber(this.state.formData.total) : ''}
+                            onChangeText={this._onAmountChanged('total')}
+                            underlineColor={Colors.inputUnderline}
+                            selectionColor={Colors.inputSelection}
+                            keyboardType={'numeric'}
+                        />
+                        <View style={styles.radioGroup}>
+                            <RadioButton.Group
+                                onValueChange={this._onChangeField('payment_type')}
+                                value={this.state.formData.payment_type}
+                            >
+                                {this._renderRadioButtons(PaymentTypes)}
+                            </RadioButton.Group>
+                        </View>
+                    </View>
+                    <View style={styles.inline}>
+                        <TextInput
+                            style={[styles.input, styles.flex2]}
+                            label='Phí ship'
+                            value={this.state.formData.ship_fee ? formatNumber(this.state.formData.ship_fee) : ''}
+                            onChangeText={this._onAmountChanged('ship_fee')}
+                            underlineColor={Colors.inputUnderline}
+                            selectionColor={Colors.inputSelection}
+                            keyboardType={'numeric'}
+                        />
+                        <View style={styles.radioGroup}>
+                            <RadioButton.Group
+                                onValueChange={this._onChangeField('ship_fee_type')}
+                                value={this.state.formData.ship_fee_type}
+                            >
+                                {this._renderRadioButtons(ShipFeeTypes)}
+                            </RadioButton.Group>
+                        </View>
+                    </View>
                 </View>
             </KeyboardAwareScrollView>
         )
     }
 
-    _onChangeText = (key) => (value) => {
+    _renderRadioButtons = (options) => Object.keys(options).map((option) => (
+        <View key={option} style={styles.inline}>
+            <RadioButton value={option}/>
+            <Text style={styles.radioLabel}>{options[option]}</Text>
+        </View>
+    ));
+
+    _onChangeField = (key) => (value) => {
         const update = {};
         update[key] = value;
         const formData = Object.assign({}, this.state.formData, update);
@@ -96,20 +125,13 @@ export default class CreateOrderForm extends React.Component {
 
     _onCustomerPhoneChanged = (customer_phone) => {
         customer_phone = customer_phone.replace(/[^0-9]/g, '');
-        const update = {customer_phone};
-        const formData = Object.assign({}, this.state.formData, update);
-        this.setState({formData});
-        this.props.onChange(formData);
+        this._onChangeField('customer_phone')(customer_phone);
     };
 
     _onAmountChanged = (key) => (amount) => {
         amount = amount.replace(/[^0-9]/g, '');
         amount = amount ? parseInt(amount) : 0;
-        const update = {};
-        update[key] = amount;
-        const formData = Object.assign({}, this.state.formData, update);
-        this.setState({formData});
-        this.props.onChange(formData);
+        this._onChangeField(key)(amount);
     };
 }
 
@@ -119,5 +141,19 @@ const styles = StyleSheet.create({
     },
     input: {
         backgroundColor: 'white',
-    }
+    },
+    flex2: {
+        flex: 2,
+    },
+    inline: {
+        flexDirection: 'row',
+    },
+    radioGroup: {
+        flex: 1,
+        flexDirection: 'column',
+        paddingLeft: 10,
+    },
+    radioLabel: {
+        marginTop: 8,
+    },
 });
